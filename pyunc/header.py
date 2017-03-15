646c06d5b51d57b3383dc295a8b0042d74abfa0a
Jon Stutters <j.stutters@ucl.ac.uk>
2017-03-15 10:38:59 +0000

    Add docstrings, remove dead code, shorten lines

--- pyunc/header.py
+++ pyunc/header.py
@@ -1,39 +1,13 @@
-from __future__ import print_function
+"""Classes representing header information."""
+
 from datetime import datetime
 from functools import partial
 import re
 
 
-class UNCHeader(object):
-    def __init__(self, info):
-        self.dicom_header = {}
-        actions = {
-            'Patient_Name=': partial(self._parse_equals, 'patient_name', str),
-            'Patient_ID=': partial(self._parse_equals, 'patient_id', str),
-            'Patient_Birth_Date=': self._parse_dob,
-            'Scan_Date=': self._parse_scan_date,
-            'Modality=': partial(self._parse_equals, 'modality', str),
-            'Scanning_Sequence=': partial(self._parse_equals, 'scanning_sequence', str),
-            'Sequence_Variant=': partial(self._parse_equals, 'sequence_variant', str),
-            'Flip_Angle=': partial(self._parse_equals, 'flip_angle', float),
-            'Slice_Thickness_mm=': partial(self._parse_equals, 'slice_thickness_mm', float),
-            'Image_Orientation_Patient_Coordinates=': partial(self._parse_split, 'image_orientation_patient_coordinates', '\\', float),
-            'Image_Position_Patient_Coordinates=': partial(self._parse_split, 'image_position_patient_coordinates', '\\', float),
-            'Intensity_Rescale_Units=': partial(self._parse_equals, 'intensity_rescale_units', str),
-            'Intensity_Rescale_Slope=': partial(self._parse_equals, 'intensity_rescale_slope', float),
-            'Intensity_Rescale_Intercept=': partial(self._parse_equals, 'intensity_rescale_intercept', float),
-            'Colour_Mapping=': partial(self._parse_equals, 'colour_mapping', str),
-            '<0x': self._parse_dicom_field
-        }
-        infoarr = info.split('\n')
-        for l in infoarr:
-            for a in actions:
-                if l.startswith(a):
-                    actions[a](l)
-        self.text = infoarr
-        self.slices = self._read_slices(infoarr)
-
 class Header(object):
+    """Base class for file and slice headers."""
+
     def _parse_dims(self, l):
         dims_value = l.split(':')[1].strip()
         dims = dims_value.split(' ')
@@ -76,7 +50,11 @@ class Header(object):
         setattr(self, attr, [converter(x) for x in l.split('=', 1)[1].split(sep)])
 
     def _parse_dicom_field(self, l):
-        exp = r'(?P<tag><0x[0-9a-f]{4},\s0x[0-9a-f]{4}>)\s(?P<data_type>[\w\s\(\)]+),(\sID)?\s(?P<id>[\w\s]+)=(?P<value>.*)'
+        exp = (
+            r'(?P<tag><0x[0-9a-f]{4},\s0x[0-9a-f]{4}>)\s'
+            r'(?P<data_type>[\w\s\(\)]+),'
+            r'(\sID)?\s(?P<id>[\w\s]+)=(?P<value>.*)'
+        )
         m = re.match(exp, l)
         if m:
             if m.group('data_type') == 'Date':
@@ -91,7 +69,6 @@ class Header(object):
         slices = []
         found_slice = False
         for l in infoarr:
-            print(l)
             if l.startswith('Echo_Time='):
                 slices.append([])
                 found_slice = True
@@ -103,7 +80,6 @@ class Header(object):
     def _do_parse(self, info, actions):
         infoarr = info.split('\n')
         for l in infoarr:
-            print(l)
             for a in actions:
                 if l.startswith(a):
                     actions[a](l)
@@ -111,33 +87,83 @@ class Header(object):
 
 
 class SliceHeader(Header):
+    """Header information for a single slice."""
     def __init__(self, info):
-        print(info)
         actions = {
-            'Echo_Time=': partial(self._parse_equals, 'echo_time', str)
-        }
-        self._do_parse(info, actions)
-
-
-class UNCHeader(Header):
-    def __init__(self, info):
-        self.dicom_header = {}
-        actions = {
-            'Patient_Name=': partial(self._parse_equals, 'patient_name', str),
-            'Patient_ID=': partial(self._parse_equals, 'patient_id', str),
-            'Patient_Birth_Date=': self._parse_dob,
-            'Scan_Date=': self._parse_scan_date,
-            'Modality=': partial(self._parse_equals, 'modality', str),
-            'Scanning_Sequence=': partial(self._parse_equals, 'scanning_sequence', str),
-            'Sequence_Variant=': partial(self._parse_equals, 'sequence_variant', str),
-            'Flip_Angle=': partial(self._parse_equals, 'flip_angle', float),
-            'Slice_Thickness_mm=': partial(self._parse_equals, 'slice_thickness_mm', float),
-            'Image_Orientation_Patient_Coordinates=': partial(self._parse_split, 'image_orientation_patient_coordinates', '\\', float),
-            'Image_Position_Patient_Coordinates=': partial(self._parse_split, 'image_position_patient_coordinates', '\\', float),
-            'Intensity_Rescale_Units=': partial(self._parse_equals, 'intensity_rescale_units', str),
-            'Intensity_Rescale_Slope=': partial(self._parse_equals, 'intensity_rescale_slope', float),
-            'Intensity_Rescale_Intercept=': partial(self._parse_equals, 'intensity_rescale_intercept', float),
-            'Colour_Mapping=': partial(self._parse_equals, 'colour_mapping', str),
+            'Echo_Time=': partial(
+                self._parse_equals,
+                'echo_time', str
+            ),
+            'Image_Position_Patient_Coordinates=': partial(
+                self._parse_split,
+                'image_position_patient_patient_coordinates', '\\', float
+            ),
             '<0x': self._parse_dicom_field
         }
         self._do_parse(info, actions)
+        self.text = info
+
+
+class UNCHeader(Header):
+    """Header information."""
+    def __init__(self, info):
+        self.dicom_header = {}
+        actions = {
+            'Patient_Name=': partial(
+                self._parse_equals,
+                'patient_name', str
+            ),
+            'Patient_ID=': partial(
+                self._parse_equals,
+                'patient_id', str
+            ),
+            'Patient_Birth_Date=': self._parse_dob,
+            'Scan_Date=': self._parse_scan_date,
+            'Modality=': partial(
+                self._parse_equals,
+                'modality', str
+            ),
+            'Scanning_Sequence=': partial(
+                self._parse_equals,
+                'scanning_sequence', str
+            ),
+            'Sequence_Variant=': partial(
+                self._parse_equals,
+                'sequence_variant', str
+            ),
+            'Flip_Angle=': partial(
+                self._parse_equals,
+                'flip_angle', float
+            ),
+            'Slice_Thickness_mm=': partial(
+                self._parse_equals,
+                'slice_thickness_mm', float
+            ),
+            'Image_Orientation_Patient_Coordinates=': partial(
+                self._parse_split,
+                'image_orientation_patient_coordinates', '\\', float
+            ),
+            'Image_Position_Patient_Coordinates=': partial(
+                self._parse_split,
+                'image_position_patient_coordinates', '\\', float
+            ),
+            'Intensity_Rescale_Units=': partial(
+                self._parse_equals,
+                'intensity_rescale_units', str
+            ),
+            'Intensity_Rescale_Slope=': partial(
+                self._parse_equals,
+                'intensity_rescale_slope', float
+            ),
+            'Intensity_Rescale_Intercept=': partial(
+                self._parse_equals,
+                'intensity_rescale_intercept', float
+            ),
+            'Colour_Mapping=': partial(
+                self._parse_equals,
+                'colour_mapping', str
+            ),
+            '<0x': self._parse_dicom_field
+        }
+        self._do_parse(info, actions)
+        self.text = info
